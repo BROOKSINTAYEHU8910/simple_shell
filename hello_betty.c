e86d95e268b9735bac5aef15a6a744724a832634
Brook <sintayehubrook1540@gmail.com>
2023-11-14 13:43:55 -0800

    first

---
+++ hello_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "shell.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is a simple program that prints
+ * "Hello, Betty!" to the console.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    printf("Hello, Betty!\n");
+    return 0;
+}
+
